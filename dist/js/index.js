4f14a45bb68e4d819dbe3c900400bd13c5a89172
Mock1ng <bayuw1123@gmail.com>
2019-09-10 20:23:54 +0700

    Initial Commit
    First commit

---
+++ dist/js/index.js
@@ -0,0 +1,23 @@
+// Dropdown
+const dropdown = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(dropdown, {
+    coverTrigger: false,
+    inDuration: 300,
+    outDuration: 300,
+    alignment: 'center'
+});
+
+// Collapsible
+$(document).ready(() => {
+    $('.collapsible').collapsible()
+});
+
+// Modals
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+// Date Picker
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+});
