917b61bb202917570d2db419ae0a4e014c73cfb9
Mock1ng <bayuw1123@gmail.com>
2019-10-24 16:18:19 +0700

    WIZARD: TINGGAL INTRO

--- dist/js/index.js
+++ dist/js/index.js
@@ -42,29 +42,29 @@ $(document).ready(function () {
 });
 
 // Chart.js
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'bar',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'My First dataset',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//         }]
+//     },
 
-    // Configuration options go here
-    options: {}
-});
+//     // Configuration options go here
+//     options: {}
+// });
 
 // Profil Menu AJAX 
 
 // Data Tables
-$('document').ready(() => {
-    $('#tabel-akun').DataTable();
-})
+// $('document').ready(() => {
+//     $('#tabel-akun').DataTable();
+// })
